e47467fca48bad6fab4b047c462e4a1bac46d01f
SumanDas3001 <sumandas3001@gmail.com>
2019-08-28 15:34:03 +0530

    Add ternary operatior

--- src/Test.js
+++ src/Test.js
@@ -3,18 +3,28 @@ import React from 'react';
 const Test = ({test}) => {
 // const Test = (props) => {
 	// const {test} = props;
+	// const testList = test.map(test => {
+	// 	if (test.age > 20){
+	// 		return(
+	// 			<div className="test" key={test.id}>
+	// 				<div>Name: {test.name}</div>
+	// 				<div>Age: {test.age}</div>
+	// 				<div>About: {test.about}</div>
+	// 			</div>
+	// 		)
+	// 	}else{
+	// 		return false;
+	// 	}
+	// })
+
 	const testList = test.map(test => {
-		if (test.age > 20){
-			return(
-				<div className="test" key={test.id}>
-					<div>Name: {test.name}</div>
-					<div>Age: {test.age}</div>
-					<div>About: {test.about}</div>
-				</div>
-			)
-		}else{
-			return false;
-		}
+		return test.age > 20 ? (
+			<div className='test' key={test.id}>
+				<div>Name: {test.name}</div>
+				<div>Age: {test.age}</div>
+				<div>About: {test.about}</div>
+			</div>
+		) : null;
 	})
 	return(
 		<div className="test-list">
