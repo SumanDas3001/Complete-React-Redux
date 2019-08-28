4823371b05614edf77e4e902b4856b48099284f9
SumanDas3001 <sumandas3001@gmail.com>
2019-08-28 15:14:14 +0530

    Add if condition

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Test from './Test';
 class App extends Component {
   state = {
     test: [
-      {name: "suman", age: 23, about: "nothing", id:1},
+      {name: "suman", age: 13, about: "nothing", id:1},
       {name: "rahul", age: 24, about: "nothing", id:2},
       {name: "subha", age: 25, about: "nothing", id:3}
     ]
--- src/Test.js
+++ src/Test.js
@@ -4,13 +4,17 @@ const Test = ({test}) => {
 // const Test = (props) => {
 	// const {test} = props;
 	const testList = test.map(test => {
-		return(
-			<div className="test" key={test.id}>
-				<div>Name: {test.name}</div>
-				<div>Age: {test.age}</div>
-				<div>About: {test.about}</div>
-			</div>
-		)
+		if (test.age > 20){
+			return(
+				<div className="test" key={test.id}>
+					<div>Name: {test.name}</div>
+					<div>Age: {test.age}</div>
+					<div>About: {test.about}</div>
+				</div>
+			)
+		}else{
+			return false;
+		}
 	})
 	return(
 		<div className="test-list">
